8590b0387e01428edab003f861b790853184cc67
Sam Jordan <sam.jordan@shopify.com>
2017-11-27 15:18:37 -0500

    extract abstractions

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -5,24 +5,62 @@ class Bottles
   end
 
   def verses(starting, ending)
-    starting.downto(ending).map {|i| verse(i)}.join("\n")
+    starting.downto(ending)
+      .map { |number| verse(number) }
+      .join("\n")
   end
 
   def verse(number)
-    case number
-    when 0
-      "No more bottles of beer on the wall, no more bottles of beer.\n"\
-      "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
-    when 1
-      "1 bottle of beer on the wall, 1 bottle of beer.\n"\
-      "Take it down and pass it around, no more bottles of beer on the wall.\n"
-    when 2
-      "2 bottles of beer on the wall, 2 bottles of beer.\n"\
-      "Take one down and pass it around, 1 bottle of beer on the wall.\n"
+    "#{quantity(number).capitalize} #{container(number)} of #{beverage} on the wall, "\
+    "#{quantity(number)} #{container(number)} of #{beverage}.\n"\
+    "#{action(number)}, "\
+    "#{quantity(successor(number))} #{container(successor(number))} of #{beverage} on the wall.\n"
+  end
+
+  private
+
+  def action(number)
+    if number == 0
+      "Go to the store and buy some more"
     else
-      "#{number} bottles of beer on the wall, #{number} bottles of beer.\n"\
-      "Take one down and pass it around, #{number-1} bottles of beer on the wall.\n"
+      "Take #{pronoun(number)} down and pass it around"
     end
+  end
+
+  def pronoun(number)
+    if number == 1
+      "it"
+    else
+      "one"
+    end
+  end
+
+  def successor(number)
+    if number == 0
+      99
+    else
+      number - 1
+    end
+  end
+
+  def container(number)
+    if number == 1
+      "bottle"
+    else
+      "bottles"
+    end
+  end
+
+  def quantity(number)
+    if number == 0
+      "no more"
+    else
+      number.to_s
+    end
+  end
+
+  def beverage
+    "beer"
   end
 
 end
